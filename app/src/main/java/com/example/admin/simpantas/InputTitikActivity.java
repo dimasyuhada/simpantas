7dbe7a362516829323107c8f9b20e1f5757e794b
admin <dimasyuhada7@gmail.com>
2018-11-04 17:05:44 +0700

    Update 2/11/2018
    - read csv
    - new update view Input titik

--- app/src/main/java/com/example/admin/simpantas/InputTitikActivity.java
+++ app/src/main/java/com/example/admin/simpantas/InputTitikActivity.java
@@ -1,7 +1,9 @@
 package com.example.admin.simpantas;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,36 +12,107 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class InputTitikActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+import com.opencsv.CSVReader;
 
-    String[] years={"2015","2016","2017","2018"};
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
-    Button btnUpload;
+public class InputTitikActivity extends AppCompatActivity{
+
+    private List<Titik> titiks = new ArrayList<>();
+    private static final String FORMAT_TIME = "hh:mm:ss a";
+
+    Button btnProcess;
     Spinner spinnerTahun;
-    TextView tvFilename;
+    Spinner spinnerBulan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_titik);
         spinnerTahun = (Spinner) findViewById(R.id.spinnerTahun);
-        tvFilename = (TextView) findViewById(R.id.titleFile);
-        btnUpload = (Button) findViewById(R.id.btnUpload);
+        spinnerBulan = (Spinner) findViewById(R.id.spinnerBulan);
+        btnProcess = (Button) findViewById(R.id.btnProcess);
 
-        ArrayAdapter<String> aa = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,years);
+
+        ArrayAdapter<CharSequence> aa = ArrayAdapter.createFromResource(this, R.array.arrayTahun, R.layout.support_simple_spinner_dropdown_item);
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerTahun.setAdapter(aa);
 
-        btnUpload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //PROSES UPLOAD FILE CSV
+        ArrayAdapter<CharSequence> ab = ArrayAdapter.createFromResource(this, R.array.arrayBulan, R.layout.support_simple_spinner_dropdown_item);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerBulan.setAdapter(ab);
+
+        btnProcess.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                processReadCSV();
+
             }
         });
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getApplicationContext(), years[position], Toast.LENGTH_LONG).show();
+    private void processReadCSV() {
+//        String pickTahun = spinnerTahun.getSelectedItem().toString();
+//        System.out.println(pickTahun);
+//        try {
+//            String csvFileString = this.getApplicationInfo().dataDir+pickTahun+".csv";
+//            File csvFile = new File(csvFileString);
+//            CSVReader reader = new CSVReader(new FileReader(pickTahun+".csv"));
+//            String [] nextLine;
+//            while ((nextLine = reader.readNext()) != null){
+//                System.out.println(nextLine[0] + nextLine[1] + "dll..");
+//            }
+//
+//        } catch (java.io.IOException e) {
+//            e.printStackTrace();
+//            Toast.makeText(this, "FILE NYA GA KETEMU GBLG!!", Toast.LENGTH_SHORT).show();
+//        }
+
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter = new SimpleDateFormat(FORMAT_TIME);
+
+        InputStream is = getResources().openRawResource(R.raw.tahun2017);
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is, Charset.forName("UTF-8"))
+        );
+
+        String line = "";
+        try{
+
+            reader.readLine();
+            while((line = reader.readLine()) != null){
+                //BAGI DATANYA DENGAN TANDA KOMA
+                String[] tokens = line.split(",");
+
+                //Baca CSV nya.
+                Titik readTitik = new Titik();
+                readTitik.setLatitude(Double.
+                        parseDouble(tokens[0]));
+                readTitik.setLongitude(Double.parseDouble(tokens[1]));
+                readTitik.setUnixTime(100);
+                readTitik.setTanggal(tokens[3]);
+                readTitik.setProvinsi(tokens[4]);
+                readTitik.setKabupaten(tokens[5]);
+                readTitik.setKecamatan(tokens[6]);
+                readTitik.setDesa(tokens[7]);
+                titiks.add(readTitik);
+
+                Log.d("InputActivity","Just Created: "+readTitik);
+
+            }
+        } catch(IOException e){
+            Log.wtf("InputActivity","Error Reading on the line " + line, e);
+            e.printStackTrace();
+        }
     }
 }
